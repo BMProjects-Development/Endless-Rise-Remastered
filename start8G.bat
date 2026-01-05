4f25ea90d14c25673993a360d096f9222711650d
OverKot <sinkotro@gmail.com>
2026-01-05 18:28:40 +0200

    Initial server files setup

---
+++ start8G.bat
@@ -0,0 +1,25 @@
+@echo off
+SET JAVA_PATH=".\jre-8.0.452.9-hotspot\bin\java.exe"
+SET RAM=8G
+%JAVA_PATH% -Xmx%RAM% -Xms%RAM% ^
+-XX:+UseG1GC ^
+-XX:+ParallelRefProcEnabled ^
+-XX:MaxGCPauseMillis=200 ^
+-XX:+UnlockExperimentalVMOptions ^
+-XX:+DisableExplicitGC ^
+-XX:+AlwaysPreTouch ^
+-XX:G1NewSizePercent=30 ^
+-XX:G1MaxNewSizePercent=40 ^
+-XX:G1HeapRegionSize=8M ^
+-XX:G1ReservePercent=20 ^
+-XX:G1HeapWastePercent=5 ^
+-XX:G1MixedGCCountTarget=4 ^
+-XX:InitiatingHeapOccupancyPercent=15 ^
+-XX:G1MixedGCLiveThresholdPercent=90 ^
+-XX:G1RSetUpdatingPauseTimePercent=5 ^
+-XX:SurvivorRatio=32 ^
+-XX:+PerfDisableSharedMem ^
+-XX:MaxTenuringThreshold=1 ^
+-jar forge-1.16.5-36.2.34.jar nogui
+
+pause
