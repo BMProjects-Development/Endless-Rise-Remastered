89b2b0a154b12dc284682619cf69e6673d3ff795
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2026-01-19 17:36:42 +0300

    Changed quests & recipes

--- kubejs/server_scripts/recipes/remove_recipes.js
+++ kubejs/server_scripts/recipes/remove_recipes.js
@@ -4,14 +4,15 @@ onEvent('recipes', event => {
   const itemsToRemove = [
     'minecraft:wooden_sword', 'minecraft:wooden_pickaxe', 'minecraft:wooden_shovel', 'minecraft:wooden_axe',
     'minecraft:stone_sword', 'minecraft:stone_pickaxe', 'minecraft:stone_shovel', 'minecraft:stone_axe', 'minecraft:stone_hoe',
-    'minecraft:golden_pickaxe', 'minecraft:golden_axe',
-    'minecraft:diamond_sword',
-    'minecraft:netherite_pickaxe', 'minecraft:netherite_axe',
+	'minecraft:iron_sword', 'minecraft:iron_pickaxe', 'minecraft:iron_shovel', 'minecraft:iron_axe', 'minecraft:iron_hoe',
+    'minecraft:golden_pickaxe', 'minecraft:golden_axe', 'minecraft:golden_sword', 'minecraft:golden_hoe',
+    'minecraft:diamond_sword', 'minecraft:diamond_hoe', 
+    'minecraft:netherite_pickaxe', 'minecraft:netherite_axe', 'minecraft:netherite_hoe',
     'appliedenergistics2:certus_quartz_sword', 'appliedenergistics2:certus_quartz_pickaxe', 'appliedenergistics2:certus_quartz_shovel', 'appliedenergistics2:certus_quartz_axe', 'appliedenergistics2:certus_quartz_hoe',
     'appliedenergistics2:nether_quartz_sword', 'appliedenergistics2:nether_quartz_pickaxe', 'appliedenergistics2:nether_quartz_shovel', 'appliedenergistics2:nether_quartz_axe', 'appliedenergistics2:nether_quartz_hoe',
     'aquaculture:neptunium_sword', 'aquaculture:neptunium_axe',
     'atum:limestone_pickaxe', 'atum:limestone_sword', 'atum:limestone_axe', 'atum:stoneguard_sword', 'atum:stoneguard_greatsword',
-	'minecraft:stone_axe', 'minecraft:golden_axe', 'minecraft:netherite_axe', 'atum:limestone_axe', 'aquaculture:neptunium_axe', 'minecraft:wooden_sword',
+	'atum:limestone_axe', 'aquaculture:neptunium_axe',
 	'aquaculture:neptunium_pickaxe',
     'minecraft:stone_shovel', 'minecraft:golden_shovel', 'minecraft:netherite_shovel', 'appliedenergistics2:certus_quartz_shovel',
     'appliedenergistics2:nether_quartz_shovel',
@@ -22,8 +23,8 @@ onEvent('recipes', event => {
     'betterendforge:terminite_shovel',
     'byg:pendorite_shovel',
     'endrem:end_crystal_shovel',
-	'minecraft:wooden_hoe', 'minecraft:stone_hoe', 'minecraft:golden_hoe', 'minecraft:diamond_hoe', 'minecraft:netherite_hoe', 'endrem:end_crystal_hoe', 'byg:pendorite_hoe', 'betterendforge:aeternium_hoe', 'betterendforge:thallasium_hoe', 'betterendforge:terminite_hoe',
-	'aquaculture:neptunium_hoe', 'atum:limestone_hoe', 'minecraft:golden_sword'
+	'endrem:end_crystal_hoe', 'byg:pendorite_hoe', 'betterendforge:aeternium_hoe', 'betterendforge:thallasium_hoe', 'betterendforge:terminite_hoe',
+	'aquaculture:neptunium_hoe', 'atum:limestone_hoe'
   ]                                                                           
 
   itemsToRemove.forEach(item => {
